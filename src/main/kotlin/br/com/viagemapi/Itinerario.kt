ace8bba7b9983d6d1ea7e3406ad72c43fd8b30d0
Yuri Coelho Brandão Carneiro <yuricoelho33@gmail.com>
2021-04-24 14:55:07 -0300

    Add files via upload
    Classe abstrata para implementação de métodos de inserção abstratos.

--- src/main/kotlin/br/com/viagemapi/Itinerario.kt
+++ src/main/kotlin/br/com/viagemapi/Itinerario.kt
@@ -1,10 +1,12 @@
 package br.com.viagemapi
 
-class Itinerario : Demografia() {
-    
+class Itinerario: Demografia(){
     var origem: String? = null
     var destino: String? = null
     var dataIda: String? = null
     var dataVolta: String? = null
+
+
+
 
 }
